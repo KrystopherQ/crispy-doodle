2372e8a096d2e8e606746f20d6e48ea8d819f4ae
Krystopher Quintero <krystopher.q@gmail.com>
2022-05-04 16:47:12 -0700

    create route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes:['id','product_name','price','stock'],
+    include:[{
+      model: Category,
+      attributes:['category_name']
+    },
+  {
+    model:Tag,
+    attributes:['tag_name']
+  }
+  ]
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where:{
+      id: req.params.id
+    },
+    attributes:['id','product_name','price','stock'],
+    include:[{
+      model:Category,
+      attributes:['category_name']
+    },
+  
+  {
+    model:Tag,
+    attributes:['tag_name']
+  }]
+  })
 });
 
 // create new product
